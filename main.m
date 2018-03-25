b84ba14ec222e3d3ed229f836e2afa0703f8deec
Athielme <36907398+Athielme@users.noreply.github.com>
2018-03-25 17:15:54 -0400

    Figure skeletons
    Adds functionality for generating figures for individual test cases, as well as a number of example/skeleton figures of these figures

--- main.m
+++ main.m
@@ -15,7 +15,7 @@ air_v = [air_v_x;air_v_y];
 air_v = transpose(air_v);
 
 water_v_x = sin([0:10]);
-water_v_x = interp1([0:10], water_v_x, times);
+water_v_x = interp1([0:10], water_v_x .* 10, times);
 
 water_v_y = [-5:10];
 water_v_y = interp1([-5:10], water_v_y, times);
@@ -29,15 +29,62 @@ berg_y = 0;
 
 counter = 1;
 for t = times
-    disp(berg_v(counter,:))
+    air_force = F_air(air_v(counter,:),berg_v(counter,:));
+    water_force = F_water(water_v(counter,:), berg_v(counter,:));
+    berg_force = air_force + water_force;
     berg_v(counter + 1,:) = berg_v(counter, :) + (del_t/berg_mass).*( ...
-          F_air(air_v(counter,:),berg_v(counter,:)) ...
-        + F_water(water_v(counter,:), berg_v(counter,:)));
+          berg_force);
     
     norm_berg(counter) = norm(berg_v(counter,:));
     berg_x(counter + 1) = berg_x(counter) + berg_v(counter + 1, 1) * del_t;
-    berg_y(counter + 1) = berg_y(counter) + berg_v(counter + 1, 1) * del_t;
+    berg_y(counter + 1) = berg_y(counter) + berg_v(counter + 1, 2) * del_t;
     counter = counter + 1;
 end
 
+%% FREE BODY DIAGRAM
+figure(1)
+quiver([0,0,0],[0,0,0], [air_force(1),water_force(1), berg_force(1)], [air_force(2),water_force(2), berg_force(2)])
+title("Free Body Diagram of Iceberg")
+xlabel("Force (N)")
+ylabel("Force (N)")
+xlim([-20 20])
+ylim([-20 20])
+
+%% ICEBERG POSITION (Small Scale)
+figure(2)
 scatter(berg_x,berg_y)
+title("Position of Iceberg (Small Scale)")
+xlabel("X Position (m)")
+ylabel("Y Position (m)")
+
+%% ICEBERG POSITION (Large Scale)
+figure(3)
+scatter(berg_x,berg_y)
+
+title("Position of Iceberg (Large Scale)")
+xlabel("X Position (km)")
+ylabel("Y Position (km)")
+
+%% Tugboat Force 
+figure(4)
+plot(times, norm_berg)
+
+title("Tugboat Force Required to Maintain Desired Course")
+xlabel("Time (hours)");
+ylabel("Force Required (N)");
+
+%% Mass loss
+figure(5)
+plot(times, [10000:-100:0])
+
+title("Iceberg Mass Loss")
+xlabel("Time (hours)")
+ylabel("Icberg Mass (kg)")
+
+%% Voyage cost
+figure(6)
+plot(times, norm_berg .* 10000)
+
+title("Cost of Voyage")
+xlabel("Time (hours)");
+ylabel("Cost ($)");
